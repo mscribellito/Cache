eecd5f86da5e2d8eead53ff24ab68ed1ef9a4c43
Michael Scribellito <mscribellito@gmail.com>
2021-02-14 14:31:04 -0500

    Firearm sorting

--- Cache/Pages/Firearms/Index.cshtml.cs
+++ Cache/Pages/Firearms/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Cache.Data;
 using Cache.Models;
+using System.Linq.Dynamic.Core;
 
 namespace Cache.Pages.Firearms
 {
@@ -19,20 +20,26 @@ namespace Cache.Pages.Firearms
             _context = context;
         }
 
+        [BindProperty(SupportsGet = true)]
+        public int CurrentPage { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SortBy { get; set; } 
+
+        public const int PageSize = 10;
+
         public PaginatedList<Firearm> Firearms { get; set; }
 
-        public async Task OnGetAsync(int? pageIndex)
+        public async Task OnGetAsync()
         {
 
-            int pageSize = 10;
             IQueryable<Firearm> firearms = from f in _context.Firearm
                 .Include(f => f.CaliberGauge)
+                .OrderBy(SortBy)
                 select f;
 
-            firearms = firearms.OrderByDescending(f => f.DateAcquired);
-
             Firearms = await PaginatedList<Firearm>.CreateAsync(
-                firearms.AsNoTracking(), pageIndex ?? 1, pageSize);
+                firearms.AsNoTracking(), CurrentPage, PageSize);
 
         }
     }
